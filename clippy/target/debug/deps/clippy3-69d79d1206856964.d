2dac606c87805aa8e723b8c5abbec6a3b168cf41
Achyuta <achyutathegreat@gmail.com>
2022-08-04 16:41:30 +0530

    upload

---
+++ clippy/target/debug/deps/clippy3-69d79d1206856964.d
@@ -0,0 +1,7 @@
+/Users/achyuta/Desktop/Code/rustlings/exercises/clippy/target/debug/deps/clippy3-69d79d1206856964.rmeta: clippy3.rs
+
+/Users/achyuta/Desktop/Code/rustlings/exercises/clippy/target/debug/deps/clippy3-69d79d1206856964.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
